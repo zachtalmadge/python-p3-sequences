3c12b69cac86f0d69778d15f58b77bfd5b1208bc
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-22 19:41:28 -0400

    pass tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    fibonacci_sequence = []
+    a, b = 0, 1
+    
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+        
+    print(fibonacci_sequence)
